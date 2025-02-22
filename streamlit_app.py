155ad28ae4f815238022f845ebc4611a56c75276
Mahendra3090 <mahendra.crv98@gmail.com>
2025-02-22 23:07:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 Bankruptcy Project")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.select_slider("Pick a size", ["S", "M", "L"])
