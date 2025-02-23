2074105530d1d4df857586528aea7b830d61efcd
Mahendra3090 <mahendra.crv98@gmail.com>
2025-02-23 07:31:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,9 @@ import os
 # Streamlit UI
 st.title("Bankruptcy Prediction App")
 st.write("Enter the required details to predict BANKRUPTCY.")# Load trained model
- 
+
+# Input fields for features
+financial_flexibility = st.number_input("Financial Flexibility", min_value=0.0, max_value=1.0, step=0.5)
+credibility = st.number_input("Credibility", min_value=0.0, max_value=1.0, step=0.5)
+competitiveness = st.number_input("Competitiveness", min_value=0.0, max_value=1.0, step=0.5)
  
